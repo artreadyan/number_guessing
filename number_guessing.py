cec09506f858e2466981f75545e25d6088e73871
Dyan <artreadyan-agilis@github.com>
2019-10-09 21:54:39 +0800

    first version of number guessing game

---
+++ number_guessing.py
@@ -0,0 +1,38 @@
+import random
+import time
+
+def generate_number():
+    random_number = random.randint(1,5)
+    return random_number
+
+def input_number():
+    guess_input = input("Guess the number! Input: ")
+    return guess_input
+
+def start_again():
+    guess_again = input("Guess again? (y/n) ")
+    if guess_again == "y":
+        guess_number()
+    else:
+        exit()
+
+def guess_number():
+    random_number = int(generate_number())
+    print("Random Number Generated")
+    time.sleep(1)
+    guess_input = int(input_number())
+
+    if guess_input < random_number:
+        print("Guessed number is less than the generated number")
+        start_again()
+
+    elif guess_input > random_number:
+        print("Guessed number is greater than the generated number")
+        start_again()   
+
+    else:
+        print("You guessed the number!")
+        start_again()
+
+while True:
+    guess_number()
